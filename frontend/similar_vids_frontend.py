076be8ad29a821aa070fac5aa2e4c5edc1ee5842
felix <felix.s@posteo.de>
2017-06-27 15:51:01 +0200

    flask now serves the bootstrap template, receives button push and text entered

---
+++ frontend/similar_vids_frontend.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from jinja2 import FileSystemLoader   
+from jinja2.environment import Environment
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template('six_similar.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():     
+    if request.form['submit'] == 'receive_text':
+        print ("button pushed")
+        text = request.form['text']
+        print (text)        
+    return ('', 204)
+
+if __name__ == '__main__':
+    app.run()
